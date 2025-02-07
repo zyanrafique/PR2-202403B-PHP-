94df31580a4950a82afb23b005d7af7635c8b5d9
zyan <zyan@gmail.com>
2025-02-07 19:06:04 +0500

    Forloop use in php

--- ForLoop.php
+++ ForLoop.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ForLoop</title>
+    <title>ForLoop in php</title>
 </head>
 <body>
    <!--  forloop sa table print kiya ha  -->
