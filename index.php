77d2a681876438b837c5ee10d6be5806add8629d
zyan <zyan@gmail.com>
2025-02-07 17:50:24 +0500

    first class Intro PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+$stdname = 'asif';
+echo $stdname;
+?>
+<h2>Hello</h2>
+<?php
+$stdAge = 23;
+ echo "<h3>".$stdAge."<h3/>";
+ echo "<h3>.$stdAge.<h3/>";
+ echo '<h3>'.$stdname.'<h3/>';
+
+ ?>
+ 
+ <h1><?php echo $stdname ?></h1>
+ <h1><?php echo $stdname . " " . $stdAge ?></h1>
+<?php
+$stdName = "Aliyan";
+$stdEmail="aliyan@gmail.com";
+$stdphone="032354635";
+ ?>
+ <h2><?php echo $stdName. "  " .$stdEmail."  ".$stdphone ?></h2>
+
+ 
